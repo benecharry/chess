58c30e3f7051dee6c588364156a8cb2009a7d2d8
benecharry <benjaminecharry@gmail.com>
2024-07-16 14:09:52 -0600

    Added the initial steps of the Register endpoint. Some other changes.

---
+++ server/src/main/java/service/Login.java
@@ -0,0 +1,6 @@
+package service;
+
+public class Login {
+    //Subset of Register
+    //Not authenticated
+}
---
+++ server/src/main/java/service/Logout.java
@@ -0,0 +1,6 @@
+package service;
+
+public class Logout {
+    //Find authToken and delete it.
+    //Add auth token.
+}
