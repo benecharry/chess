fea3709f58f2e0ac6540df8503535f812649d3ac
benecharry <benjaminecharry@gmail.com>
2024-07-30 20:30:02 -0600

    Added the landing page of the ChessClient on the petshop including the quit method. For lack of creativity/time/willingness, I based the model on the one given in the project description.

--- client/src/main/java/client/ChessClient.java
+++ client/src/main/java/client/ChessClient.java
@@ -1,5 +1,69 @@
 package client;
 
-public class ChessClient {
+import exception.ResponseException;
+import request.RegisterRequest;
+import result.RegisterResult;
+import server.ServerFacade;
+import static ui.EscapeSequences.*;
 
+import java.util.Arrays;
+
+public class ChessClient {
+    private State state = State.LOGGEDOUT;
+    private final ServerFacade server;
+    private final PreloginUI preloginUI;
+
+    public ChessClient(String serverUrl, PreloginUI preloginUI) {
+        this.server = new ServerFacade(serverUrl);
+        this.preloginUI = preloginUI;
+    }
+
+    public String eval(String input) {
+        try {
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "quit" -> "quit";
+                default -> help();
+            };
+        } catch (ResponseException ex) {
+            return ex.getMessage();
+        }
+    }
+
+    public String quit(){
+        return "quit";
+    }
+
+    public String help() {
+        if (state == State.LOGGEDOUT) {
+            return String.format("""
+                    %sregister <USERNAME> <PASSWORD> <EMAIL>%s - to create an account
+                    %slogin <USERNAME> <PASSWORD>%s - to play chess
+                    %squit%s - playing chess
+                    %shelp%s - with possible commands
+                    """,
+                    SET_TEXT_COLOR_BLUE, RESET_TEXT_COLOR,
+                    SET_TEXT_COLOR_BLUE, RESET_TEXT_COLOR,
+                    SET_TEXT_COLOR_BLUE, RESET_TEXT_COLOR,
+                    SET_TEXT_COLOR_BLUE, RESET_TEXT_COLOR);
+        }
+        return String.format("""
+                %screate <NAME>%s - a game
+                %slist%s - games
+                %sjoin <ID> <WHITE|BLACK>%s - a game
+                %sobserve <ID>%s - a game
+                %slogout%s - when you are done
+                %squit%s - playing chess
+                %shelp%s - with possible commands
+                """,
+                SET_TEXT_COLOR_BLUE, RESET_TEXT_COLOR,
+                SET_TEXT_COLOR_BLUE, RESET_TEXT_COLOR,
+                SET_TEXT_COLOR_BLUE, RESET_TEXT_COLOR,
+                SET_TEXT_COLOR_BLUE, RESET_TEXT_COLOR,
+                SET_TEXT_COLOR_BLUE, RESET_TEXT_COLOR,
+                SET_TEXT_COLOR_BLUE, RESET_TEXT_COLOR,
+                SET_TEXT_COLOR_BLUE, RESET_TEXT_COLOR);
+    }
 }
--- client/src/main/java/client/PreloginUI.java
+++ client/src/main/java/client/PreloginUI.java
@@ -1,7 +1,41 @@
 package client;
 
+
+import java.util.Scanner;
+import static ui.EscapeSequences.*;
+
 public class PreloginUI {
     private final ChessClient client;
 
+    public PreloginUI(String serverUrl) {
+        client = new ChessClient(serverUrl, this);
+    }
 
+    public void run(){
+        System.out.println("\uD83D\uDC51 Welcome to 240 chess. Type Help to get started. \uD83D\uDC51");
+        System.out.print(client.help());
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = client.eval(line);
+                if (result.equals("quit")) {
+                    break;
+                }
+                System.out.print(result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(SET_TEXT_COLOR_RED + msg + RESET_TEXT_COLOR);
+            }
+        }
+        System.out.println();
+    }
+
+    private void printPrompt() {
+        System.out.print("\n" + RESET_TEXT_COLOR + ">>> " + SET_TEXT_COLOR_GREEN);
+    }
 }
